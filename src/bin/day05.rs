7565751d81a747b99a25b57e8fe8ecd48d34f050
Miguel Mano <miguelmanofernandes@gmail.com>
2021-12-05 18:22:37 +0000

    Refactor draw_line method

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
-use std::{cmp::min, collections::HashMap};
+use std::collections::HashMap;
 
-#[derive(Debug)]
+#[derive(Clone)]
 struct Vent {
     start: (i32, i32),
     end: (i32, i32),
@@ -26,8 +26,12 @@ fn main() {
         })
         .collect();
 
-    println!("P1: {}", draw_lines(&vents, false));
-    println!("P2: {}", draw_lines(&vents, true));
+    let (mut straight, mut diagonals): (Vec<Vent>, Vec<Vent>) =
+        vents.into_iter().partition(|vent| is_straight_line(vent));
+    println!("P1: {}", generate_ocean_floor(&straight));
+
+    straight.append(&mut diagonals);
+    println!("P2: {}", generate_ocean_floor(&straight));
 }
 
 fn is_straight_line(vent: &Vent) -> bool {
@@ -38,37 +42,26 @@ fn get_overlaps(ocean: &Ocean) -> usize {
     ocean.values().filter(|vent| *vent >= &2).count()
 }
 
-fn draw_lines(vents: &[Vent], use_diagonals: bool) -> usize {
+fn draw_line(ocean: &mut Ocean, vent: &Vent) {
+    let (dx, dy) = (
+        i32::signum(vent.end.0 - vent.start.0),
+        i32::signum(vent.end.1 - vent.start.1),
+    );
+
+    let mut curr = vent.start;
+    *ocean.entry(curr).or_insert(0) += 1;
+
+    while curr.0 != vent.end.0 || curr.1 != vent.end.1 {
+        curr = (curr.0 + dx, curr.1 + dy);
+        *ocean.entry(curr).or_insert(0) += 1;
+    }
+}
+
+fn generate_ocean_floor(vents: &[Vent]) -> usize {
     let mut ocean: Ocean = HashMap::new();
 
     for vent in vents.iter() {
-        if is_straight_line(vent) {
-            let (dx, dy) = (
-                i32::abs(vent.start.0 - vent.end.0),
-                i32::abs(vent.start.1 - vent.end.1),
-            );
-
-            let (sx, sy) = (min(vent.start.0, vent.end.0), min(vent.start.1, vent.end.1));
-
-            (sx..=sx + dx).for_each(|x| {
-                (sy..=sy + dy).for_each(|y| {
-                    *ocean.entry((x, y)).or_insert(0) += 1;
-                })
-            });
-        } else if use_diagonals {
-            let (dx, dy) = (
-                i32::signum(vent.end.0 - vent.start.0),
-                i32::signum(vent.end.1 - vent.start.1),
-            );
-
-            let mut curr = vent.start;
-            *ocean.entry(curr).or_insert(0) += 1;
-
-            while curr.0 != vent.end.0 && curr.1 != vent.end.1 {
-                curr = (curr.0 + dx, curr.1 + dy);
-                *ocean.entry(curr).or_insert(0) += 1;
-            }
-        }
+        draw_line(&mut ocean, vent);
     }
 
     get_overlaps(&ocean)
