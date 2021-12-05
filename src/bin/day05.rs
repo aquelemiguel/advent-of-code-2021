5af1e72ebb31c64329bc3422657b2eed9674a55c
Miguel Mano <miguelmanofernandes@gmail.com>
2021-12-05 21:21:11 +0000

    Add .scan()

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,6 +1,7 @@
 use regex::Regex;
 use std::collections::HashMap;
 
+#[derive(Clone)]
 struct Vent {
     start: (i32, i32),
     end: (i32, i32),
@@ -12,30 +13,31 @@ fn main() {
     let input = std::fs::read_to_string("input/d05-full").expect("Error while reading");
     let re = Regex::new(r"(\d+),(\d+) -> (\d+),(\d+)").unwrap();
 
-    let vents = input.lines().map(|line| {
-        let capture = re.captures_iter(line).next().unwrap();
-        let coords: Vec<i32> = (1..5).map(|i| capture[i].parse().unwrap()).collect();
+    let vents: Vec<Vent> = input
+        .lines()
+        .map(|line| {
+            let capture = re.captures_iter(line).next().unwrap();
+            let coords: Vec<i32> = (1..5).map(|i| capture[i].parse().unwrap()).collect();
 
-        Vent {
-            start: (coords[0], coords[1]),
-            end: (coords[2], coords[3]),
-        }
-    });
+            Vent {
+                start: (coords[0], coords[1]),
+                end: (coords[2], coords[3]),
+            }
+        })
+        .collect();
 
-    let (mut straight, mut diagonals): (Vec<Vent>, Vec<Vent>) =
-        vents.partition(|vent| is_straight_line(vent));
-    println!("P1: {}", generate_ocean_floor(&straight));
+    let straight: Vec<Vent> = vents
+        .clone()
+        .into_iter()
+        .filter(|vent| is_straight_line(vent))
+        .collect();
 
-    straight.append(&mut diagonals);
-    println!("P2: {}", generate_ocean_floor(&straight));
+    println!("P1: {}", get_overlaps(&straight));
+    println!("P2: {}", get_overlaps(&vents));
 }
 
 fn is_straight_line(vent: &Vent) -> bool {
     vent.start.0 == vent.end.0 || vent.start.1 == vent.end.1
-}
-
-fn get_overlaps(ocean: &Ocean) -> usize {
-    ocean.values().filter(|vent| *vent >= &2).count()
 }
 
 fn draw_line(ocean: &mut Ocean, vent: &Vent) {
@@ -44,21 +46,29 @@ fn draw_line(ocean: &mut Ocean, vent: &Vent) {
         i32::signum(vent.end.1 - vent.start.1),
     );
 
-    let mut curr = vent.start;
-    *ocean.entry(curr).or_insert(0) += 1;
+    let steps = i32::max(
+        i32::abs(vent.start.0 - vent.end.0),
+        i32::abs(vent.start.1 - vent.end.1),
+    );
 
-    while curr.0 != vent.end.0 || curr.1 != vent.end.1 {
-        curr = (curr.0 + dx, curr.1 + dy);
-        *ocean.entry(curr).or_insert(0) += 1;
+    let points: Vec<(i32, i32)> = (0..steps)
+        .scan(vent.start, |state, _| {
+            *state = (state.0 + dx, state.1 + dy);
+            Some(*state)
+        })
+        .collect();
+
+    for point in [points.as_slice(), &[vent.start]].concat() {
+        *ocean.entry(point).or_insert(0) += 1;
     }
 }
 
-fn generate_ocean_floor(vents: &[Vent]) -> usize {
+fn get_overlaps(vents: &[Vent]) -> usize {
     let mut ocean: Ocean = HashMap::new();
 
     for vent in vents.iter() {
         draw_line(&mut ocean, vent);
     }
 
-    get_overlaps(&ocean)
+    ocean.values().filter(|vent| *vent >= &2).count()
 }
